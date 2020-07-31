ac9cdca153aaa4bbd2e7c5606d286e2fbd32f3fd
Talgat Muratbekov <talgat2307@gmail.com>
2020-08-01 02:19:04 +0600

    #2 - Created BreadTop.js and BreadBottom.js components

---
+++ src/components/ingredients/BreadBottom.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BreadBottom = () => {
+  return (
+    <div className='BreadBottom'>
+    </div>
+  )
+}
+
+export default BreadBottom
---
+++ src/components/ingredients/BreadTop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BreadTop = () => {
+  return (
+    <div className='BreadTop'>
+      <div className='Seeds1'></div>
+      <div className='Seeds2'></div>
+    </div>
+  )
+}
+
+export default BreadTop
